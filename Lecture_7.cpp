46c3279fd79d37da5c68fd463fa4c40bd55453fd
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-03-03 15:33:40 +0500

    Update Lecture_7.cpp

---
+++ Lecture_7.cpp
@@ -0,0 +1,13 @@
+/*
+    using Pointers to Objects
+    13.7 Constructors
+        Using member initializer lists
+        inplace member initialization
+        Default constructors
+        Default constructor and dynamically allocated objects
+        Passing arguments to constructors
+        using default arguments to the constructor
+        Destructors 
+
+
+ */
