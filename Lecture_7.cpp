24e24e24f8322ba2430189c62beeefcb1b7d9ae4
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-03-04 11:45:11 +0500

    lect 7

--- Lecture_7.cpp
+++ Lecture_7.cpp
@@ -15,3 +15,52 @@
         using private member functions     
 
  */
+#include <iostream>
+using namespace std;
+class Triange
+{
+    private:
+        int base=0;
+        int height=0;
+    public:
+        Triange():base(10), height(10)
+        {
+            
+        }
+        Triange(int b, int h)
+        {
+            base = b;
+            height = h;
+        }
+
+       
+        void  setBase(int b)
+        {
+            base = b;
+        }
+        void setHeight(int h)
+        {
+            height = h;
+        }
+        void getBase() const
+        {
+            cout << "Base is: " << base << endl;
+            cout<< "Height is: " << height << endl;
+        }
+        int getHeight() 
+        {
+            
+            return height;
+        }
+};
+int main()
+{
+    Triange t1;
+    Triange t2;
+    t1.getBase();
+    t1.setBase(100);
+    t1.getBase();
+    double height =t1.getHeight();
+
+    return 0;
+}
