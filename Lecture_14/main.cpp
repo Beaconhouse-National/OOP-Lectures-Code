c1dd2c9e3efc8054ff6d949ec712ef72b96b7b42
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-03-25 20:11:34 +0500

    lecture 14

--- Lecture_14/main.cpp
+++ Lecture_14/main.cpp
@@ -1,46 +1,75 @@
 #include<iostream>
-#include<string>
 using namespace std;
-class Student{
+
+class Student {
     private:
-    string name;
-    int age;
+        int id;
+        string name;
+        int status;
+        
     public:
-    Student(){
-        name = "";
-        age = 0;
-    }
-    Student(string n, int a){
-        name = n;
-        age = a;
-    }
-    void setName(string n){
-        name = n;
-        this->name = n;
-        (*this).name = n;
-        //this is a pointer to the object that is calling the function
-    }
-    void setAge(int a){
-        age = a;
-    }
-    string getName() const{
-        return name;
-    }
-    int getAge() const{
-        return age;
-    }
-    void print(){
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-    }
-    void print() const{
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-    }
+        Student(){
+            id = 0;
+            name = "";
+            status = 0;
+        }
+        void setId(int i){
+            id = i;
+        }
+        void setName(string n){
+            name = n;
+        }
+        void setStatus(int s){
+            status = s;
+        }
+        int getId() const{
+            return id;
+        }
+        string getName() const{
+            return name;
+        }
+        int getStatus() const{
+            return status;
+        }
+        void print(){
+            cout << "ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Status: " << status << endl;
+        }
+};
+class Account{
+    private:
+        double balance;
+    public:
+        Account(){
+            balance = 0;
+        }
+        void setBalance(double b){
+            balance = b;
+            this->balance = b;
+            (*this).balance = b;
+        }
+        double getBalance() const{
+            return balance;
+        }
+        void deposit(double amount){
+            balance += amount;
+        }
+        void withdraw(double amount){
+            balance -= amount;
+        }
+        void changestatus(Student &s){
+            if (balance >0)
+            s.setStatus(1);
+        }
 };
 int main(){
     Student s1;
-    Student s2;
+    s1.setId(1);
     s1.setName("Ali");
+    Account a1;
+
+    a1.setBalance( 11000);
+    a1.changestatus(s1);
     return 0;
 }
